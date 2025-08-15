cb2e91d3609ab90764d08d2e53493647b0b44793
Axel Closas <axelclosas13@gmail.com>
2025-08-15 20:58:31 -0300

    Agregar archivo dbConfig.mjs para la conexión a MongoDB

---
+++ config/dbConfig.mjs
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+export async function connectDB() {
+  try {
+    await mongoose.connect(process.env.DB_URL)
+    console.log('Conexión exitosa a MongoDB ')
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error)
+    process.exit(1)
+  }
+}
